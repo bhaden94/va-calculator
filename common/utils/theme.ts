3b77f5e3cccacb39d74c835b516fb53968c5c18c
bhaden94 <bhaden94@yahoo.com>
2023-04-13 11:23:17 -0400

    Change colors and add overrides for autocomplete.

--- common/utils/theme.ts
+++ common/utils/theme.ts
@@ -7,16 +7,16 @@ export const theme: Theme = createTheme({
 	palette: {
 		contrastThreshold: 4.5,
 		primary: {
-			main: "#d8c690",
+			main: "#fff",
 			light: "#d8c690",
 			dark: "#BE9E44",
-			contrastText: 'rgba(0, 0, 0, 0.87)',
+			contrastText: "rgba(0, 0, 0, 0.87)",
 		},
 		secondary: {
 			main: "#000",
 			light: "#d8c690",
 			dark: "#BE9E44",
-			contrastText: 'rgba(0, 0, 0, 0.87)',
+			contrastText: "rgba(0, 0, 0, 0.87)",
 		},
 	},
 	components: {
@@ -24,7 +24,7 @@ export const theme: Theme = createTheme({
 			styleOverrides: {
 				root: {
 					background: `url('./${BLACK_GRADIENT_NAME}') no-repeat center center fixed`,
-					color: "#fff"
+					color: "#fff",
 				},
 			},
 		},
@@ -51,6 +51,24 @@ export const theme: Theme = createTheme({
 				},
 				secondary: {
 					color: "#000",
+				},
+			},
+		},
+		MuiAutocomplete: {
+			styleOverrides: {
+				root: {
+					"& label.Mui-focused": {
+						color: "#1a1a1a",
+					},
+					"& .MuiInput-underline:after": {
+						borderBottomColor: "#1a1a1a",
+					},
+					"& .MuiOutlinedInput-root": {
+						"&.Mui-focused fieldset": {
+							borderColor: "#1a1a1a",
+							borderWidth: 1,
+						},
+					},
 				},
 			},
 		},
