17cf51c155a2856798d48cf9d47e7733254c6531
bhaden94 <bhaden94@yahoo.com>
2023-04-19 08:45:25 -0400

    Add todo.

--- common/components/HomePriceInput/HomePriceInput.tsx
+++ common/components/HomePriceInput/HomePriceInput.tsx
@@ -7,6 +7,7 @@ export const HomePriceInput: FC = () => {
 	const { originalLoanAmountState, newHomePriceState } =
 		useEntitlementDataInput();
 
+	// TODO: Add information bubble on text field to explain the inputs
 	return (
 		<div style={{ width: "100%" }}>
 			<InputFormField
