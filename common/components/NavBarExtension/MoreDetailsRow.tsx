e5226ebcffd9d4c743807f6a109dcf346d5ad0d7
bhaden94 <bhaden94@yahoo.com>
2023-04-23 15:03:34 -0400

    remove todo's.

--- common/components/NavBarExtension/MoreDetailsRow.tsx
+++ common/components/NavBarExtension/MoreDetailsRow.tsx
@@ -10,7 +10,6 @@ interface IMoreDetailsRowProps {
 	informationBubbleTitle?: string;
 }
 
-// TODO: make into a styled component
 export const MoreDetailsRow: FC<IMoreDetailsRowProps> = ({
 	title,
 	amount,
