b8ec0796e56d9dc0df2956a4b7a8f6edd5c0aa81
bhaden94 <bhaden94@yahoo.com>
2023-05-19 16:16:29 -0400

    Blog post changed to styled component and better heading control.

--- common/components/SharedComponents/BlogPost.tsx
+++ common/components/SharedComponents/BlogPost.tsx
@@ -1,19 +1,30 @@
-import { Box, SxProps, Theme } from "@mui/material";
+import { Box, styled } from "@mui/material";
 import { FC } from "react";
 
 interface IBlogPostProps {
 	children: React.ReactNode; // Blog posts will come from the blog-posts directory
 }
 
-export const BlogPost: FC<IBlogPostProps> = ({ children }) => {
-	const blogStyles: SxProps<Theme> = {
+const StyledBlogPost = styled(Box)(({ theme }) =>
+	theme.unstable_sx({
 		padding: {
 			xs: "50px 25px",
 			sm: "2rem 1rem",
 			md: "3rem 3rem",
 			lg: "4rem 5rem",
 		},
-	};
+		"& h2": {
+			fontSize: "2rem",
+		},
+		"& h3": {
+			fontSize: "1.5rem",
+		},
+		"& h4": {
+			fontSize: "1.17rem",
+		},
+	})
+);
 
-	return <Box sx={blogStyles}>{children}</Box>;
+export const BlogPost: FC<IBlogPostProps> = ({ children }) => {
+	return <StyledBlogPost>{children}</StyledBlogPost>;
 };
