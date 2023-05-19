cdd9d9b25564f53e87da491c6be7552bdef99718
bhaden94 <bhaden94@yahoo.com>
2023-05-19 15:46:20 -0400

    sort imports.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import { Analytics } from '@vercel/analytics/react';
+import type { AppProps } from "next/app";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
