92ffb012e16c94a6866e752403a243a55ea0d18c
bhaden94 <bhaden94@yahoo.com>
2024-08-25 14:12:13 -0400

    Fix spelling issue and unused import.

--- features/second-tier-entitlement/utils/localData.ts
+++ features/second-tier-entitlement/utils/localData.ts
@@ -1,7 +1,4 @@
-import {
-  EntitlementDataRow,
-  OptimizedEntitlementData,
-} from "../types/EntitlementModel";
+import { EntitlementDataRow } from "../types/EntitlementModel";
 import Papa from "papaparse";
 import fs from "fs";
 import path from "path";
@@ -11,11 +8,12 @@ const DATA_FILE_NAME_2023: string = "2023_us_entitlement_data.csv";
 const DATA_FILE_NAME_2024: string = "2024_us_entitlement_data.csv";
 
 export const getEntitlementData = (): EntitlementDataRow[] => {
-  const filePath = path.join(process.cwd(), DATA_FOLDER, DATA_FILE_NAME_2024);
-  // Keep syncronous so we don't have file reading issues with multiple requests
-  const fileContent = fs.readFileSync(filePath, "utf-8");
-  const { data } = Papa.parse<EntitlementDataRow>(fileContent, {
-    header: true,
-  });
-  return data;
+	const filePath = path.join(process.cwd(), DATA_FOLDER, DATA_FILE_NAME_2024);
+	// Keep synchronous so we don't have file reading issues with multiple requests
+	const fileContent = fs.readFileSync(filePath, "utf-8");
+	const { data } = Papa.parse<EntitlementDataRow>(fileContent, {
+		header: true,
+	});
+	return data;
+	``;
 };
