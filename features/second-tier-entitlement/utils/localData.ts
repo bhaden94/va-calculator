3c6af5243172b0230f76ab27b1073a81eba633a2
bhaden94 <bhaden94@yahoo.com>
2025-01-11 15:26:55 -0500

    Add 2025 second tier entitlement data.

--- features/second-tier-entitlement/utils/localData.ts
+++ features/second-tier-entitlement/utils/localData.ts
@@ -6,9 +6,10 @@ import path from "path";
 const DATA_FOLDER: string = "data";
 const DATA_FILE_NAME_2023: string = "2023_us_entitlement_data.csv";
 const DATA_FILE_NAME_2024: string = "2024_us_entitlement_data.csv";
+const DATA_FILE_NAME_2025: string = "2025_us_entitlement_data.csv";
 
 export const getEntitlementData = (): EntitlementDataRow[] => {
-  const filePath = path.join(process.cwd(), DATA_FOLDER, DATA_FILE_NAME_2024);
+  const filePath = path.join(process.cwd(), DATA_FOLDER, DATA_FILE_NAME_2025);
   // Keep synchronous so we don't have file reading issues with multiple requests
   const fileContent = fs.readFileSync(filePath, "utf-8");
   const { data } = Papa.parse<EntitlementDataRow>(fileContent, {
