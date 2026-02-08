f67074b9f5b65672e899fa43f48f6822386da173
bhaden94 <bhaden94@yahoo.com>
2026-02-08 09:55:35 -0500

    Include 2026 data and add logic to automatically get the latest data once available.

--- app/api/second-tier-entitlement/route.ts
+++ app/api/second-tier-entitlement/route.ts
@@ -4,30 +4,30 @@ import Fuse, { IFuseOptions } from "fuse.js";
 import { EntitlementDataRow } from "../../../features/second-tier-entitlement/types/EntitlementModel";
 
 export async function GET(req: NextRequest) {
-  const url = new URL(req.url);
-  const queryString = url.searchParams
-    .get("query")
-    ?.replaceAll(" ", "")
-    .split(",");
+	const url = new URL(req.url);
+	const queryString = url.searchParams
+		.get("query")
+		?.replaceAll(" ", "")
+		.split(",");
 
-  const nonOptimized = getEntitlementData();
+	const nonOptimized = getEntitlementData(new Date().getFullYear());
 
-  const options: IFuseOptions<EntitlementDataRow> = {
-    includeScore: true,
-    threshold: 0.2,
-    distance: 50,
-    useExtendedSearch: true,
-    keys: ["zipCode", "county", "state", "entitlement"],
-  };
-  const fuse = new Fuse<EntitlementDataRow>(nonOptimized, options);
-  // Advanced search splits parts of the zip code, county, and state
-  // to perform an 'or' query
-  const searchResult = queryString
-    ? fuse.search(`${queryString.join("|")}`)
-    : [];
+	const options: IFuseOptions<EntitlementDataRow> = {
+		includeScore: true,
+		threshold: 0.2,
+		distance: 50,
+		useExtendedSearch: true,
+		keys: ["zipCode", "county", "state", "entitlement"],
+	};
+	const fuse = new Fuse<EntitlementDataRow>(nonOptimized, options);
+	// Advanced search splits parts of the zip code, county, and state
+	// to perform an 'or' query
+	const searchResult = queryString
+		? fuse.search(`${queryString.join("|")}`)
+		: [];
 
-  // Limit returned array size and extract the EntitlementDataRow from the list
-  const result = searchResult.slice(0, 25).map((item) => item.item);
+	// Limit returned array size and extract the EntitlementDataRow from the list
+	const result = searchResult.slice(0, 25).map((item) => item.item);
 
-  return NextResponse.json(result);
+	return NextResponse.json(result);
 }
--- features/second-tier-entitlement/utils/localData.ts
+++ features/second-tier-entitlement/utils/localData.ts
@@ -4,17 +4,49 @@ import fs from "fs";
 import path from "path";
 
 const DATA_FOLDER: string = "data";
-const DATA_FILE_NAME_2023: string = "2023_us_entitlement_data.csv";
-const DATA_FILE_NAME_2024: string = "2024_us_entitlement_data.csv";
-const DATA_FILE_NAME_2025: string = "2025_us_entitlement_data.csv";
+const DATA_FILE_NAME: string = "{year}_us_entitlement_data.csv";
 
-export const getEntitlementData = (): EntitlementDataRow[] => {
-  const filePath = path.join(process.cwd(), DATA_FOLDER, DATA_FILE_NAME_2025);
-  // Keep synchronous so we don't have file reading issues with multiple requests
-  const fileContent = fs.readFileSync(filePath, "utf-8");
-  const { data } = Papa.parse<EntitlementDataRow>(fileContent, {
-    header: true,
-  });
-  return data;
-  ``;
+/**
+ * Gets the entitlement data for the most recent year, starting with the passed in year param.
+ * Checks if a file exists for the given year, and if not, decrements the year until it finds a file or reaches 2023.
+ * @param year The year to attempt getting data for.
+ * @returns The full data set for the most recent year available, starting with the passed in year param.
+ */
+export const getEntitlementData = (year: number): EntitlementDataRow[] => {
+	// Test if file exists for the given year
+	// If not, decrement one year until we find a file or reach 2023
+	let fileExists = false;
+	let currentYear = year;
+
+	while (!fileExists && currentYear >= 2023) {
+		const filePath = path.join(
+			process.cwd(),
+			DATA_FOLDER,
+			DATA_FILE_NAME.replace("{year}", currentYear.toString()),
+		);
+		if (fs.existsSync(filePath)) {
+			fileExists = true;
+		} else {
+			currentYear--;
+		}
+	}
+
+	if (!fileExists) {
+		throw new Error(
+			`No data file found for year ${year} or any previous year down to 2023.`,
+		);
+	}
+
+	const filePath = path.join(
+		process.cwd(),
+		DATA_FOLDER,
+		DATA_FILE_NAME.replace("{year}", currentYear.toString()),
+	);
+
+	// Keep synchronous so we don't have file reading issues with multiple requests
+	const fileContent = fs.readFileSync(filePath, "utf-8");
+	const { data } = Papa.parse<EntitlementDataRow>(fileContent, {
+		header: true,
+	});
+	return data;
 };
